159281d941d3f9443423b3f171711ed8c62c8d81
christot20 <thomaschristo1234@yahoo.com>
2021-11-29 17:01:57 -0500

    added backspace feature

--- martian.py
+++ martian.py
@@ -11,9 +11,9 @@ print("Initializing...")
 time.sleep(2)
 print("Hey Watney! Nice of you to try and communicate with us.")
 time.sleep(2)
-print("The commands are:")
+print("The commands are:" +"\n")
 time.sleep(1)
-print("Stop: Shuts down program." + "\n"+"Reset: Clears past letters and words."+"\n"+"End: Combines letters to make word."+"\n"+"Sentence: Takes words to formulate message.")
+print("Stop: Shuts down program." + "\n"+"Reset: Clears past letters and words."+"\n"+"End: Combines letters to make word."+"\n"+"Sentence: Takes words to formulate message."+"\n"+"Del: Backspace."+"\n")
 time.sleep(2)
 print("You shouldn't have numbers from 1-1F or anything higher than 7F: ")
 #add a delete feature for the writing of letters using pop function
@@ -38,12 +38,15 @@ def hex_to_words():
             hex_to_words()
         elif hex_num.lower() == "sentence":
             print(" ".join(messageArray))
+        elif hex_num.lower() == "del":
+            letArray.pop()
+        elif hex_num.lower() == "help":
+            print("Stop: Shuts down program." + "\n"+"Reset: Clears past letters and words."+"\n"+"End: Combines letters to make word."+"\n"+"Sentence: Takes words to formulate message."+"\n"+"Del: Backspace."+"\n")
         else:
             if len(hex_num) <= 2:
                 if any(x not in hex_allowed_chars for x in hex_num):
                     print("error: invalid number")
                     hex_to_words()
-                print("condi1")
                 letter = chr((int(hex_num,16)))
                 print(letter)
                 if any(x not in allowed_characters for x in letter):
